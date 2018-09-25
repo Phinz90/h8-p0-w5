2fd5d9442cbad8a81a1cf5ea9bcd6813129fdc6d
Yosephine Mellyana <42544640+Phinz90@users.noreply.github.com>
2018-09-25 17:03:21 +0700

    Add files via upload

---
+++ hapusSimbol.js
@@ -0,0 +1,23 @@
+//Diberikan sebuah function hapusSimbol(str) yang menerima satu parameter berupa string. Function akan me-return string yang telah bersih dari berbagai simbol, hanya menyisakan a-z dan angka 0-9.
+
+function hapusSimbol(str) {
+    // e("[^a-zA-Z0-9]+")
+    let finalStr = '';
+    let newStr = str.match(/[a-z0-9]+/gi);
+    
+    let result = newStr.toString();
+    
+    for (let i=0; i<newStr.length; i++) {
+        if (newStr[i] !== ',') {
+            finalStr += newStr[i];
+        }
+    }
+    
+    return finalStr;
+}
+// TEST CASES
+console.log(hapusSimbol('test%$4aa')); // test4aa
+console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
+console.log(hapusSimbol('ma@#k!an~')); // makan
+console.log(hapusSimbol('coding')); // coding
+console.log(hapusSimbol('1+3-5*2=100')); // 1352100
